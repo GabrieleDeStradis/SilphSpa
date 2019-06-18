2ff0e97fd1e37054e261b1561f8af314bf9ac025
Gabriele <gabriele.destradis.ing@gmail.com>
2019-06-18 16:19:42 +0200

    attempting fixing resources path

--- src/main/java/it/uniroma3/siw/silphspa/SilphSpaApplication.java
+++ src/main/java/it/uniroma3/siw/silphspa/SilphSpaApplication.java
@@ -1,6 +1,7 @@
 package it.uniroma3.siw.silphspa;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +12,14 @@ public class SilphSpaApplication {
 	public static void main(String[] args) {
 		/* all'avvio viene creata la cartella dove verra' gestita la galleria di immagini*/
 		/*System.getProperty("user.dir")+"/src/main/resources/static/*/
-		new File("/downloads_silph/").mkdir();
+		try {
+			String creation_path = (new File(".").getCanonicalPath())+"/src/main/resources/static/images/";
+			new File(creation_path).mkdir();
+			System.out.println("PATH DI CREAZIONE:\n"+creation_path);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		SpringApplication.run(SilphSpaApplication.class, args);
 	}
 
